e52ff480264c95afede3dc8e82d4c4e05b26ef74
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-04-20 16:31:36 +0900

    edit header design

--- frontend/src/component/Header/header.js
+++ frontend/src/component/Header/header.js
@@ -22,43 +22,45 @@ const Header = ({ setActiveMainContent }) => {
             <div className="space-x-6 pt-3 mb-3 mt-2">
                 <div className="flex">
                     </div>
-                        <div className="flex mt-5 font-bold text-lg">
-                            <Link to="/" rel="noopener noreferrer">
-                                <div className="flex">
-                                    <img
-                                        src={wargame} alt="wargame" className="w-8 h-8 ml-5 hover:cursor-pointer"
-                                        onClick={() => {
-                                            setSelectedCategory("Main");
-                                            setActiveMainContent("Main");
-                                        }}
-                                    />
-                                    <p
-                                        className="text-lg ml-3"
-                                        onClick={() => {
-                                            setSelectedCategory("Main");
-                                            setActiveMainContent("Main");
-                                        }}
-                                    >
-                                        WargameSolve
-                                    </p>
-                                </div>
-                            </Link>
-                            {categories.map((category, index) => (
+                        <div className="flex justify-between mt-5 font-bold text-lg">
+                            <div className="flex">
                                 <Link to="/" rel="noopener noreferrer">
-                                    <p 
-                                        key={index}
-                                        className={`${index > 0 ? 'ml-8' : 'ml-12'} ${selectedCategory === category ? 'text-black' : 'text-gray-400'} hover:text-black hover:cursor-pointer`}
-                                        onMouseEnter={handleCategoryToggle}
-                                        onClick={() => {
-                                            handleCategoryClick(category);
-                                        }}
-                                    >
-                                        {category}
-                                    </p>
+                                    <div className="flex">
+                                        <img
+                                            src={wargame} alt="wargame" className="w-8 h-8 ml-5 hover:cursor-pointer"
+                                            onClick={() => {
+                                                setSelectedCategory("Main");
+                                                setActiveMainContent("Main");
+                                            }}
+                                        />
+                                        <p
+                                            className="text-lg ml-3"
+                                            onClick={() => {
+                                                setSelectedCategory("Main");
+                                                setActiveMainContent("Main");
+                                            }}
+                                        >
+                                            WargameSolve
+                                        </p>
+                                    </div>
                                 </Link>
-                            ))}
+                                {categories.map((category, index) => (
+                                    <Link to="/" rel="noopener noreferrer">
+                                        <p 
+                                            key={index}
+                                            className={`${index > 0 ? 'ml-8' : 'ml-12'} ${selectedCategory === category ? 'text-black' : 'text-gray-400'} hover:text-black hover:cursor-pointer`}
+                                            onMouseEnter={handleCategoryToggle}
+                                            onClick={() => {
+                                                handleCategoryClick(category);
+                                            }}
+                                        >
+                                            {category}
+                                        </p>
+                                    </Link>
+                                ))}
+                            </div>
                             <Link to="/login" rel="noopener noreferrer">
-                                <p className="fixed bg-blue-600 p-2 rounded-lg text-sm right-10 text-white hover:cursor-pointer hover:underline">
+                                <p className="-mt-1 mr-10 bg-blue-600 p-2 rounded-lg text-sm text-white hover:cursor-pointer hover:underline">
                                     로그인
                                 </p>
                             </Link>

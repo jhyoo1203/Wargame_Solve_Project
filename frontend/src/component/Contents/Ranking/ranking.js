7fc938d2801304580c89e377c29ed29a9e4ce46c
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-04-20 16:28:43 +0900

    create ranking component

---
+++ frontend/src/component/Contents/Ranking/ranking.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Rank = () => {
+    return(
+        <div>
+            <div className="flex justify-center items-center h-screen bg-blue-200">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Welcome to Team 0100!</h1>
+                    <p className="text-xl mt-3">This is the Ranking of Team 0100's web application.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Rank;
--- frontend/src/component/Contents/content.js
+++ frontend/src/component/Contents/content.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./Main/main";
 import Wargame from "./Wargame/wargame"
+import Rank from "./Ranking/ranking";
 import Community from "./Community/community";
 
 const Content = ({ activeMainContent }) => {
@@ -8,7 +9,7 @@ const Content = ({ activeMainContent }) => {
     <div>
       {activeMainContent === "Main" && <Main />}
       {activeMainContent === "Wargame" && <Wargame />}
-      {/* {activeMainContent === "Ranking" && <Rank />} */}
+      {activeMainContent === "Ranking" && <Rank />}
       {activeMainContent === "Community" && <Community />}
     </div>
   );

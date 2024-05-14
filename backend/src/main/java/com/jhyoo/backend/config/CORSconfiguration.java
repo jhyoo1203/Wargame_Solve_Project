932a020056a58884a4ddb181aecf822565bfefee
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-05-14 14:04:36 +0900

    CORS setting

---
+++ backend/src/main/java/com/jhyoo/backend/config/CORSconfiguration.java
@@ -0,0 +1,25 @@
+package com.jhyoo.backend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSconfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://www.gwnueum.com", "http://gwnueum.com" ,"http://localhost:2019", "*")
+                        .allowedMethods("GET", "POST", "PATCH")
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
